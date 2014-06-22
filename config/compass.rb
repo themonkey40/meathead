8bf5db4f5314efe386eed799fcfd6aec5e7a49ab
Mark Horton <mphorton@gmail.com>
2014-06-22 13:36:14 -0700

    updated config

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,8 @@
 # Require any additional compass plugins here.
 project_type = :rails
+project_path = Compass::AppIntegration::Rails.root
+
+http_path = "/"
 
 css_dir = "app/assets/stylesheets"
 sass_dir = "app/assets/stylesheets/scss"

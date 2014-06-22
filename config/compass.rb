91eee53a728782a351e77ebd6cbccc68c7386c49
Mark Horton <mphorton@gmail.com>
2014-06-22 15:00:54 -0700

    heroku config

--- config/compass.rb
+++ config/compass.rb
@@ -2,7 +2,7 @@
 project_type = :rails
 
 http_path    = '/'
-css_dir = "tmp/stylesheets"
+css_dir = "app/assets/stylesheets"
 sass_dir = "app/assets/stylesheets/scss"
 images_dir = "app/assets/images"
 javascripts_dir = "app/assets/javascripts"

6dc6c4d7aacec268c125dc9aa0cf817960bd5ce2
Mark Horton <mphorton@gmail.com>
2014-06-22 21:26:10 -0700

    fixed parallax issue and updated contact section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         $('*[class^="prlx"]').each(function(r){
             var pos = $(this).offset().top;
             var scrolled = $(window).scrollTop();
-            $('*[class^="prlx"]').css('top', -(scrolled * 0.6) + 'px');         
+            $('*[class^="prlx"]').css('top', -(scrolled * 0.5) + 'px');         
         });
     });
 });

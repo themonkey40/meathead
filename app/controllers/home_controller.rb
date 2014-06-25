5f2b08daaac297c0b66ffc56823fae934e97af19
Mark Horton <mphorton@gmail.com>
2014-06-24 20:16:17 -0700

    added emailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,15 @@
 class HomeController < ApplicationController
+	def index
+	end
+
+	def create
+		@email = params
+		CateringMailer.catering_email.deliver
+
+		redirect_to root_path
+	end
+	private
+    def email_params
+      params.permit(:name, :email, :phone, :message)
+    end
 end
---
+++ app/mailers/catering_mailer.rb
@@ -0,0 +1,13 @@
+class CateringMailer < ActionMailer::Base
+  default from: "info@meatheadtruck.com"
+  
+  	def catering_email
+		delivery_options = { user_name: 'info@meatheadtruck.com',
+							password: 'Oakland24',
+							address: 'mail.privateemail.com' }
+		mail(to: 'mphorton@gmail.com',
+			subject: 'New Catering Request From Meatheadtruck.com',
+			delivery_method_options: delivery_options)
+	end
+
+end

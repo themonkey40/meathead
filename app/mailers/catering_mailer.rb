e963ad36fe4f22d65a3e1d54855995cd48b0216f
Mark Horton <mphorton@gmail.com>
2015-01-13 17:32:15 -0800

    udpated email settings

--- app/mailers/catering_mailer.rb
+++ app/mailers/catering_mailer.rb
@@ -3,7 +3,7 @@ class CateringMailer < ActionMailer::Base
   
   	def catering_email (email)
 		@email = email
-		delivery_options = { user_name: 'info@meatheadtruck.com', password: 'Oakland24', address: 'mail.privateemail.com' }
+		delivery_options = { user_name: 'daniel@meatheadtruck.com', password: 'Comeon2424', address: 'smtp.gmail.com' }
 		mail(to: 'daniel@meatheadtruck.com', subject: 'New Catering Request From Meatheadtruck.com', delivery_method_options: delivery_options)
 	end
 
